2e0954efb4f214813bb2de508f0eb0388895abec
DeathCatx <56968554+DeathCatx@users.noreply.github.com>
2021-07-15 14:40:09 +0800

    deploy

---
+++ precache-manifest.a67b262c09ca4fe3a57efcbf92ceda76.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e038c47d55463169ac6",
+    "url": "/covid-dashboard/css/about.751c108c.css"
+  },
+  {
+    "revision": "f68b987dc09b46e13325",
+    "url": "/covid-dashboard/css/app.545c13da.css"
+  },
+  {
+    "revision": "36ab62da7f5a769a789a",
+    "url": "/covid-dashboard/css/chunk-vendors.3de502ed.css"
+  },
+  {
+    "revision": "665b63746d9b3e7a304a3ea8c1c847ce",
+    "url": "/covid-dashboard/favicon.png"
+  },
+  {
+    "revision": "1990c9a3a25ba631be11fb439bdc37c6",
+    "url": "/covid-dashboard/img/coronavirus.1990c9a3.svg"
+  },
+  {
+    "revision": "d8022a7511cbaf1af6436ad401a02cec",
+    "url": "/covid-dashboard/img/dark_mode_black_24dp.d8022a75.svg"
+  },
+  {
+    "revision": "1c4c9ee2577f14185e801ad62e1a440d",
+    "url": "/covid-dashboard/img/heartbeat.1c4c9ee2.svg"
+  },
+  {
+    "revision": "39592beb56c625a7f7023e5ac8eaa9b8",
+    "url": "/covid-dashboard/img/icon-info-light.39592beb.svg"
+  },
+  {
+    "revision": "3b26c0a1f3542f232084e250aaadf9df",
+    "url": "/covid-dashboard/img/icon-info.3b26c0a1.svg"
+  },
+  {
+    "revision": "109609f716488734fdecda93d668f842",
+    "url": "/covid-dashboard/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "32e24752fd62929887ae001a2cd64f98",
+    "url": "/covid-dashboard/img/icons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "b3956a3c696ed13f89fe9bf145e3e493",
+    "url": "/covid-dashboard/img/icons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "665b63746d9b3e7a304a3ea8c1c847ce",
+    "url": "/covid-dashboard/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "83535ac326bf023f7a01ed622f53f292",
+    "url": "/covid-dashboard/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "382601f1d729916b7dfa45a4dc7a8a8e",
+    "url": "/covid-dashboard/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "56e1bedaeaf12edaa635f90d9047c466",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f2304f15d69807206e0a7afa0923aa49",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "1f2c002876d907d0408d3745f394485e",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "849ddbb5f10582b2c5db69588df3338d",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "817d0f76b77973e565b7398926de6235",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "1f2c002876d907d0408d3745f394485e",
+    "url": "/covid-dashboard/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "9bb93aab6aa5a52f4f64e0a38dfddfa1",
+    "url": "/covid-dashboard/img/icons/browserconfig.xml"
+  },
+  {
+    "revision": "4f94b50fc76fa16bdbe5ea75f1eba9e1",
+    "url": "/covid-dashboard/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "f2fc131ce84656b1bb7e7c1dcc50e6fc",
+    "url": "/covid-dashboard/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "df34dffac9fce147558438ce53a5b594",
+    "url": "/covid-dashboard/img/icons/manifest.webmanifest"
+  },
+  {
+    "revision": "f475fe2758797bba5c1aa5b12b3bf19b",
+    "url": "/covid-dashboard/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "b3258df3c64d6940d657a3d4b9c95f05",
+    "url": "/covid-dashboard/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "bccd5bd4d0a8949365293ed7578acf91",
+    "url": "/covid-dashboard/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "74534d32b2ae0de172ee98cf32b0aee8",
+    "url": "/covid-dashboard/img/light_mode_white_24dp.74534d32.svg"
+  },
+  {
+    "revision": "f316beaea4abf4b601f6f924f69b6df9",
+    "url": "/covid-dashboard/img/recover.f316beae.svg"
+  },
+  {
+    "revision": "a69f534eda8a53befc2529c65796ed05",
+    "url": "/covid-dashboard/img/tombstone.a69f534e.svg"
+  },
+  {
+    "revision": "ec6e22a3f60a066f289c3e1da3d6e637",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "7e038c47d55463169ac6",
+    "url": "/covid-dashboard/js/about.4f1ec237.js"
+  },
+  {
+    "revision": "f68b987dc09b46e13325",
+    "url": "/covid-dashboard/js/app.e78648fe.js"
+  },
+  {
+    "revision": "36ab62da7f5a769a789a",
+    "url": "/covid-dashboard/js/chunk-vendors.825dafda.js"
+  },
+  {
+    "revision": "72a8a39a468829bbf1d2a6629a9e505e",
+    "url": "/covid-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-dashboard/robots.txt"
+  }
+]);
